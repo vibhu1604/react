e8952a3dfd83543d1a16dd2613067d9057107268
Vibhuti Gohil <vibhutigohil77@gmail.com>
2023-04-19 12:08:45 -0400

    Just design

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,42 @@
-
-import './App.css';
+import { useState } from 'react';
+import './index.css';
 import Employee from './components/Employee';
+import { v4 as uuidv4 } from 'uuid';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  const [role, setRole] = useState("Dev");
+  const [employees, setEmployees] = useState(
+    [
+      {
+
+        name: "Vibhuti",
+        role: "Front End Developer",
+        img: "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg",
+      },
+      {
+        name: "Saloni",
+        role: "Lab Assistant",
+        img: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg",
+      },
+      {
+        name: "Sagar",
+        role: "QA",
+        img: "https://images.pexels.com/photos/1987301/pexels-photo-1987301.jpeg",
+      },
+      {
+        name: "Dip",
+        role: "Operator",
+        img: "https://images.pexels.com/photos/718978/pexels-photo-718978.jpeg",
+      },
+      {
+        name: "Jose",
+        role: "Web Developer",
+        img: "https://images.pexels.com/photos/3831645/pexels-photo-3831645.jpeg",
+      },
+    ]
+  );
+
   console.log('We re about to list the employees');
   const showEmployees = true;
   return (
@@ -10,13 +44,21 @@ function App() {
     <div className="App">
       {console.log('We re in show empl')}
       {showEmployees ?
-        <div>
-          <Employee />
-          <Employee />
-          <Employee />
-          <Employee />
-          <Employee />
+        <div className='flex flex-wrap justify-center'>
+          {employees.map(
+            (employee) => {
+              return (
+                <Employee
+                  key={uuidv4()}
+                  name={employee.name}
+                  role={employee.role}
+                  img={employee.img}
+                />
+              )
+            }
+          )}
         </div>
+
         :
         <p>You can not see the employees</p>
       }
--- src/components/Employee.jsx
+++ src/components/Employee.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import EditEmployee from './EditEmployee'
 
-function Employee() {
+function Employee(props) {
+
     return (
-        <div>
-            Employee
+        <div className="py-8 px-8 max-w-sm m-2 bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
+            <img className="object-cover rounded-full h-[100px] w-[100px] object-center block mx-auto h-24 sm:mx-0 sm:shrink-0" src={props.img} alt="Woman's Face" />
+            <div className="text-center space-y-2 sm:text-left">
+                <div className="space-y-0.5">
+                    <p className="text-lg text-black font-semibold">
+                        {props.name}
+                    </p>
+                    <p className="text-slate-500 font-medium">
+                        {props.role}
+                    </p>
+                </div>
+                <EditEmployee />
+
+            </div>
         </div>
     )
 }

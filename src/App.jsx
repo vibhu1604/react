10de60a2d0edc55b0b4d5f0ca9f7347853bfb174
Vibhuti Gohil <vibhutigohil77@gmail.com>
2023-04-19 01:27:01 -0400

    getting ready for css

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,25 @@
 
 import './App.css';
+import Employee from './components/Employee';
 
 function App() {
+  console.log('We re about to list the employees');
+  const showEmployees = true;
   return (
-    <div className="App">
-      <header className="App-header">
 
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App">
+      {console.log('We re in show empl')}
+      {showEmployees ?
+        <div>
+          <Employee />
+          <Employee />
+          <Employee />
+          <Employee />
+          <Employee />
+        </div>
+        :
+        <p>You can not see the employees</p>
+      }
     </div>
   );
 }
